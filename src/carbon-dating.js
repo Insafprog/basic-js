e6f0711649afce09771997c4f7ecf78793c67379
Insafprog <insaf@gmx.ru>
2022-04-25 16:41:44 +0300

    :feat carbon-dating count-cats dream-team

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== "string") return false
+  const result = Math.ceil(Math.log(MODERN_ACTIVITY/+sampleActivity)/Math.LN2*HALF_LIFE_PERIOD)
+  if(result !== Infinity && result > 0) return result
+  else return false
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,11 +11,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (date === undefined) return 'Unable to determine the time of year!'
+  try {
+    const month = date.getMonth()
+    if (month === 11 || month < 2) return 'winter'
+    if (1 < month && month < 5) return 'spring'
+    if (4 < month && month < 8) return 'summer'
+    if (7 < month && month < 11) return 'autumn'
+  } catch (e) {
+    throw new Error("Invalid date!");
+  }
 }
 
 module.exports = {
   getSeason
 };
+
+console.log(getSeason(() => new Date()))
